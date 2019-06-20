ef08ede99f372bd99325c3d288660c9082862d67
Christian Ampe <christianampe.work@gmail.com>
2019-06-20 09:25:37 -0700

    Added back the all tests property to avoid breaking changes.

--- Tests/TyreTests/TyreTests.swift
+++ Tests/TyreTests/TyreTests.swift
@@ -2,14 +2,5 @@ import XCTest
 @testable import Tyre
 
 final class TyreTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Tyre().text, "Hello, World!")
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    static var allTests = []
 }
